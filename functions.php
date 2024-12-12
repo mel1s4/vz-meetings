3dc37beee7f0917a38fdfb192168573ef4c255ae
Melisa Viroz <102311661+mel1s4@users.noreply.github.com>
2024-12-12 09:18:01 -0800

    Initial Commit

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Viroz Appointment Manager
+Plugin URI: https://viroz.studio/vz-appointment-manager
+Description: This plugin will help you to sell and manage your appointments for services and products.
+Version: 0.0.1
+Author: Melisa Viroz
+Author URI: https://melisaviroz.com
+License: GPL2
+*/
